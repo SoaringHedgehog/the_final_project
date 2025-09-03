9f71fd160eab428dd911ab1153bd6e7fdd61f062
smartHedgehog-dev <elarionovnof@gmail.com>
2025-09-03 18:56:26 +0400

    Реализация параллельной сортировки

--- src/main/java/algorithm/SelectionSort.java
+++ src/main/java/algorithm/SelectionSort.java
@@ -1,4 +1,23 @@
 package algorithm;
 
-public class SelectionSort {
+import java.util.ArrayList;
+
+public class SelectionSort<T extends Comparable<T>> {
+    // Метод для сортировки выбором
+    public void sort(ArrayList<T> arr) {
+        for (int i = 0; i < arr.size() - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.size(); j++) {
+                if (arr.get(j).compareTo(arr.get(minIndex)) < 0) {
+                    minIndex = j;
+                }
+            }
+            // Меняем местами
+            if (minIndex != i) {
+                T temp = arr.get(i);
+                arr.set(i, arr.get(minIndex));
+                arr.set(minIndex, temp);
+            }
+        }
+    }
 }
