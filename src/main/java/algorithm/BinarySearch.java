fcbf65fe7b567bf0618a0c7c3c9e67160cac1f47
ArteShock <klyew.1998@gmail.com>
2025-09-04 17:05:17 +0400

    Добавлен бинарный поиск объекта

--- src/main/java/algorithm/BinarySearch.java
+++ src/main/java/algorithm/BinarySearch.java
@@ -1,5 +1,22 @@
 package algorithm;
 
+import java.util.ArrayList;
+
 public class BinarySearch {
 
+    public static Integer indexByObject(ArrayList<Comparable> sortedArray, Comparable requiredObject){
+        int leftIndex = 0;
+        int rightIndex = sortedArray.size() - 1 ;
+        while(leftIndex <= rightIndex){
+            int midIndex = leftIndex + (rightIndex - leftIndex) / 2;
+            if(sortedArray.get(midIndex).compareTo(requiredObject) == 0){
+                return midIndex;
+            } else if (sortedArray.get(midIndex).compareTo(requiredObject) < 0) {
+                leftIndex = midIndex + 1;
+            } else {
+                rightIndex = midIndex - 1;
+            }
+        }
+        return -1;
+    }
 }
