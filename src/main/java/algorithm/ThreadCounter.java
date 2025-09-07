a8056c2e4cd2152a875bd320815b448595a15f5b
Olesya Frolova <olesya.eryshkanova@gmail.com>
2025-09-08 00:29:30 +0300

    Добавлен ThreadCounter с выводом результата

---
+++ src/main/java/algorithm/ThreadCounter.java
@@ -0,0 +1,43 @@
+package algorithm;
+import java.util.*;
+
+public class ThreadCounter {
+    public static <T> int countOccurrences(List<T> list, T element, int numThreads) throws InterruptedException {
+        int size = list.size();
+        int elementsPerThread = (int) Math.ceil((double) size / numThreads);
+
+        int[] results = new int[numThreads];
+
+        Thread[] threads = new Thread[numThreads];
+
+        for (int i = 0; i < numThreads; i++) {
+            int start = i * elementsPerThread;
+            int end = Math.min(start + elementsPerThread, size);
+            int threadIndex = i;
+
+            threads[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    int count = 0;
+                    for (int j = start; j < end; j++) {
+                        if (list.get(j).equals(element)) {
+                            count++;
+                        }
+                    }
+                    results[threadIndex] = count;
+                }
+            });
+            threads[i].start();
+        }
+        for (int i = 0; i < threads.length; i++) {
+            threads[i].join();
+        }
+        int total = 0;
+
+        for (int i = 0; i < results.length; i++) {
+            total = total + results[i];
+        }
+        System.out.println("Элемент \"" + element + "\" встречается " + total + " раз(а).");
+        return total;
+    }
+}
