7098ce8b790aba92fd7928ec68f4d6aa255160b6
shirinova-ra <120368784+shirinova-ra@users.noreply.github.com>
2025-09-08 10:57:05 +0300

    Update User.java

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,6 +1,5 @@
 package entity;
 
-
 public class User implements Comparable<User> {
     private final String name;
     private final String password;
@@ -13,12 +12,20 @@ public class User implements Comparable<User> {
     }
 
     public String getName() { return name; }
+
     public String getPassword() { return password; }
+
     public String getEmail() { return email; }
 
     @Override
-    public int compareTo(User user) {
-        return this.name.compareTo(user.name);
+    public int compareTo(User other) {
+        int result = this.name.compareTo(other.name);
+        if (result != 0) return result;
+
+        result = this.password.compareTo(other.password);
+        if (result != 0) return result;
+
+        return this.email.compareTo(other.email);
     }
 
     public static class Builder {
@@ -31,24 +38,27 @@ public class User implements Comparable<User> {
                 throw new IllegalArgumentException("Введите имя");
             this.name = name;
             return this;
-    }
+        }
+
         public Builder setPassword(String password) {
             if (password == null || password.isEmpty())
                 throw new IllegalArgumentException("Введите пароль");
             this.password = password;
             return this;
-    }
+        }
+
         public Builder setEmail(String email) {
             if (email == null || email.isEmpty())
                 throw new IllegalArgumentException("Введите почту");
             this.email = email;
             return this;
-    }
+        }
+
         public User build() {
             if (name == null || password == null || email == null) {
                 throw new IllegalStateException("Необходимо указать имя, пароль и почту");
             }
             return new User(this);
+        }
     }
-}
 }
