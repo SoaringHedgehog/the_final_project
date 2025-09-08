0fdec529a6c84b07eb413c4cd330ed7f002bab1e
shirinova-ra <120368784+shirinova-ra@users.noreply.github.com>
2025-09-08 10:57:35 +0300

    Update Student.java

--- src/main/java/entity/Student.java
+++ src/main/java/entity/Student.java
@@ -1,56 +1,64 @@
 package entity;
 
+public class User implements Comparable<User> {
+    private final String name;
+    private final String password;
+    private final String email;
 
-public class Student implements Comparable<Student> {
-
-    private final int groupNumber;
-    private final double averageScore;
-    private final String gradeBookNumber;
-
-    private Student(Builder builder) {
-        this.groupNumber = builder.groupNumber;
-        this.averageScore = builder.averageScore;
-        this.gradeBookNumber = builder.gradeBookNumber;
+    private User(Builder builder) {
+        this.name = builder.name;
+        this.password = builder.password;
+        this.email = builder.email;
     }
 
-    public int getGroupNumber() { return groupNumber; }
+    public String getName() { return name; }
 
-    public double getAverageScore() { return averageScore; }
+    public String getPassword() { return password; }
 
-    public String getGradeBookNumber() { return gradeBookNumber; }
+    public String getEmail() { return email; }
 
     @Override
-    public int compareTo(Student student) {
-        return Integer.compare(this.groupNumber, student.groupNumber);
+    public int compareTo(User other) {
+        int result = this.name.compareTo(other.name);
+        if (result != 0) return result;
+
+        result = this.password.compareTo(other.password);
+        if (result != 0) return result;
+
+        return this.email.compareTo(other.email);
     }
 
     public static class Builder {
-    private int groupNumber;
-    private double averageScore;
-    private String gradeBookNumber;
+        private String name;
+        private String password;
+        private String email;
 
-    public Builder setGroupNumber(int groupNumber) {
-        if (groupNumber <= 0) throw new IllegalArgumentException("Номер группы должен быть положительным");
-        this.groupNumber = groupNumber;
-        return this;
-    }
-    public Builder setAverageScore(double averageScore) {
-        if (averageScore < 0 || averageScore > 5)
-            throw new IllegalArgumentException("Средний балл должен быть в диапазоне от 0 до 5");
-        this.averageScore = averageScore;
-        return this;
-    }
-    public Builder setRecordBookNumber(String gradeBookNumber) {
-        if (gradeBookNumber == null || gradeBookNumber.isEmpty())
-            throw new IllegalArgumentException("Укажите номер зачетной книжки");
-        this.gradeBookNumber = gradeBookNumber;
-        return this;
-    }
-    public Student build() {
-        if (gradeBookNumber == null || gradeBookNumber.isEmpty() || groupNumber <= 0 || averageScore < 0 || averageScore > 5) {
-            throw new IllegalStateException("Необходимо указать номер зачетной книжки, положительный номер группы и средний балл 0-5");
+        public Builder setName(String name) {
+            if (name == null || name.isEmpty())
+                throw new IllegalArgumentException("Введите имя");
+            this.name = name;
+            return this;
         }
-        return new Student(this);
-    }
+
+        public Builder setPassword(String password) {
+            if (password == null || password.isEmpty())
+                throw new IllegalArgumentException("Введите пароль");
+            this.password = password;
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            if (email == null || email.isEmpty())
+                throw new IllegalArgumentException("Введите почту");
+            this.email = email;
+            return this;
+        }
+
+        public User build() {
+            if (name == null || password == null || email == null) {
+                throw new IllegalStateException("Необходимо указать имя, пароль и почту");
+            }
+            return new User(this);
+        }
     }
 }
